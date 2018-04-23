5458b9aa1c89a7c8a96f979a3efde514f761d516
Kirkpatrick <kirkp1ia@cmich.edu>
2018-04-23 15:40:24 -0400

    Add documentation

--- triggers.sql
+++ triggers.sql
@@ -28,7 +28,7 @@ END;
 /
 show errors
 
--- (for department table only)
+-- (for dept_location table only)
 CREATE OR REPLACE TRIGGER dept_loc_trigger
 AFTER INSERT ON DEPT_LOCATIONS
 BEGIN
@@ -37,7 +37,7 @@ END;
 /
 show errors
 
--- (for department table only)
+-- (for project table only)
 CREATE OR REPLACE TRIGGER proj_trigger
 AFTER INSERT ON PROJECT
 BEGIN
@@ -47,7 +47,7 @@ END;
 /
 show errors
 
--- (for department table only)
+-- (for works_on table only)
 CREATE OR REPLACE TRIGGER works_on_trigger
 AFTER INSERT ON WORKS_ON
 BEGIN
@@ -56,7 +56,7 @@ END;
 /
 show errors
 
--- (for department table only)
+-- (for dependant table only)
 CREATE OR REPLACE TRIGGER depend_trigger
 AFTER INSERT ON DEPENDANT
 BEGIN
