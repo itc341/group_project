b76e41a1d068b1a21c902c11b3035f545289e33a
Kirkpatrick <kirkp1ia@cmich.edu>
2018-04-23 16:38:39 -0400

    Make package work

--- package.sql
+++ package.sql
@@ -1,6 +1,7 @@
 
 
 
+
 set linesize 120;
 set echo on;
 set serveroutput on;
@@ -11,10 +12,9 @@ set timing on;
 -- 2. remove a dependent for any given valid employee
 
 -- **** Package Declaration **** --
-
 create or replace package emp_package as
-    procedure add_emp( ssn in dependent.essn%type, dep in DEPENDANT.dependant_name%type, s in dependant.sex%type, bd in dependant.bdate%type, rel in dependant.relationship%type );
-    procedure rem_emp( ssn in dependent.essn%type, dep in DEPENDANT.dependant_name%type );
+    procedure add_dep( ssn in DEPENDANT.essn%type, dep in DEPENDANT.dependant_name%type, s in DEPENDANT.sex%type, bd in DEPENDANT.bdate%type, rel in DEPENDANT.relationship%type );
+    procedure rem_dep( ssn in DEPENDANT.essn%type, dep in DEPENDANT.dependant_name%type );
 end;
 /
 show errors
@@ -22,12 +22,12 @@ show errors
 -- **** Package Body **** --
 create or replace package body emp_package as
 
-    procedure add_emp( ssn in dependent.essn%type, dep in DEPENDANT.dependant_name%type, s in dependant.sex%type, bd in dependant.bdate%type, rel in dependant.relationship%type );
+    procedure add_dep( ssn in DEPENDANT.essn%type, dep in DEPENDANT.dependant_name%type, s in DEPENDANT.sex%type, bd in DEPENDANT.bdate%type, rel in DEPENDANT.relationship%type )
 	as
 		test INT;
 	begin
 
-		select e.ssn into test from EMPLOYEE e and DEPENDANT d where e.ssn = d.essn;
+		select count(*) into test from EMPLOYEE e where ssn = e.ssn;
 	
 		if(test = null) then
 			dbms_output.put_line('Employee not found');
@@ -42,15 +42,15 @@ create or replace package body emp_package as
     end;
 
 
-	procedure rem_emp( ssn in dependent.essn%type, dep in DEPENDANT.dependant_name%type );
+	procedure rem_dep( ssn in DEPENDANT.essn%type, dep in DEPENDANT.dependant_name%type )
 	as 
 		test INT;
 	begin
 
-		select e.ssn into test from EMPLOYEE e and DEPENDANT d where e.ssn = d.essn;
-		
+		select COUNT(*) into test from DEPENDANT d where ssn = d.essn and dep = dependant_name;
+	
 		if(test = null) then
-			dbms_output.put_line('Employee not found');
+			dbms_output.put_line('Dependant or employee not found');
 		else
 			delete from DEPENDANT d where d.dependant_name = dep;
 		end if;
